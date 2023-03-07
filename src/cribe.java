6a997926bce4b3b1700a68f1f9c5cb6cb5140268
claudiaortiz79 <aluort0593@ieselcaminas.org>
2023-03-07 23:06:48 +0100

    elimina0y1

--- src/cribe.java
+++ src/cribe.java
@@ -11,9 +11,9 @@ public class cribe
             boolean[] esPrimo = getPrimo(dim);
 // Inicializar el array
             for (i=0; i<dim; i++)
-                esPrimo[i] = true;
+                Eliminar0y1(esPrimo, i, true);
 // Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
+            Eliminar0y1(esPrimo, 0, esPrimo[1] = false);
 // Criba
             for (i=2; i<Math.sqrt(dim)+1; i++) {
                 if (esPrimo[i]) {
@@ -40,10 +40,14 @@ public class cribe
         }
     }
 
+    private static void Eliminar0y1(boolean[] esPrimo, int x, boolean esPrimo1) {
+        esPrimo[x] = esPrimo1;
+    }
+
     private static void elimprimos(int i, int dim, boolean[] esPrimo) {
         int j;
         for (j=2* i; j< dim; j+= i)
-            esPrimo[j] = false;
+            Eliminar0y1(esPrimo, j, false);
     }
 
     public static boolean[] getPrimo(int dim) {
